a40c9a45ef9a3ca8233e362671ac2edb20d64880
say.f <omigun3@icloud.com>
2021-02-07 16:49:15 +0900

    [Add]java_practice4

---
+++ JavaWcp/src/Chapter04.java
@@ -0,0 +1,11 @@
+
+public class Chapter04 {
+	public static void main(String[] args) {
+		final int LUCKY_NUMBER = 6919;
+		String str1 = "さーや";
+		System.out.println(str1);
+		str1 = "ドナルドダック";
+		System.out.println(str1);
+		System.out.println(LUCKY_NUMBER);
+	}
+}
